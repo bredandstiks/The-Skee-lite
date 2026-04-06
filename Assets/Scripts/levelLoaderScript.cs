c743ce960d5a4c70974be5aa1fd5358262904a7d
bredandstiks <raj.aarohan@gmail.com>
2026-04-06 12:01:38 -0700

    Halfway thru global inputs

--- Assets/Scripts/levelLoaderScript.cs
+++ Assets/Scripts/levelLoaderScript.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 public class levelLoaderScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
